658047c34a8c0d761c331e3f204a06a2b1fd0d83
CodeFoxZ37 <130168476+CodeFoxZ37@users.noreply.github.com>
2025-06-21 22:11:45 -0400

    Se creo la carpeta de componentes y dos nuevo componentes y se esta instalando tailwindcss en el proyecto ademas de que se removio la carpeta node_modules del git

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
-function PasswordInput({ value, onChange }) {
-  return (
-    <div>
-      <h3>Introduce tu contraseña</h3>
-      <input type="password" value={value}>
-         </input>
-    </div>
-  )
-  }
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <passwordInput><PasswordInput value={count} onChange={setCount} /></passwordInput>
     </>
   )
 }
---
+++ src/components/PasswordInput.jsx
@@ -0,0 +1,13 @@
+function PasswordInput({ value, onChange }) {
+  return (
+    <>
+    <div>
+      <h3>Introduce tu contraseña</h3>
+      <input type="password" value={value}>
+         </input>
+    </div>
+    </>
+  )
+  }
+
+export default PasswordInput;
